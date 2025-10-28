35f12ba6ba6c98f0860c456a4269fcb5da6b5adb
Hlib Poliakov <pavuk@Hlibs-MacBook-Air.local>
2025-10-28 19:31:50 +0100

    redis prod debug

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import { createApp } from "./common/createApp.js";
+import createRedisClient from "./common/createRedisClient.js";
+import { getContext } from "./context.js";
 import shutdown from "./utils/shutDown.js";
 
 const PORT = process.env.PORT || 4000;
 
 const startServer = async () => {
   const app = await createApp();
+
+  const { redisClient } = getContext();
+
+  await redisClient.set("key", "value");
+  const value = await redisClient.get("key");
+  console.log(value, "redis");
 
   app.listen(PORT, () => {
     console.log(`✅ Server running on port ${PORT}`);
